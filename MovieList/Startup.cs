458ab6ad08920dc4d70bfc0ff53829e2a4b26cd9
AncientTangeloes <119451157+AncientTangeloes@users.noreply.github.com>
2023-09-27 11:09:09 -0500

    Add project files.

---
+++ MovieList/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace MovieList
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
